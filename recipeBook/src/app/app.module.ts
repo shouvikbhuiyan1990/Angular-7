9dd6a49611de5259b5baa9eddadceb564b66cc7e
Shouvik Bhuiyan <shouvik.bhuiyan@sapient.com>
2019-02-26 15:03:16 +0530

    init

--- recipeBook/src/app/app.module.ts
+++ recipeBook/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeItemComponent } from './recipes-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
     RecipesListComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
--- recipeBook/src/app/recipes-list/recipes-list.component.ts
+++ recipeBook/src/app/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.less']
 })
 export class RecipesListComponent implements OnInit {
-
+  Recipes: Recipe[] = [
+    new Recipe('title1', 'the first item in the list', 'https://image.shutterstock.com/display_pic_with_logo/136306/722718082/stock-photo-healthy-food-clean-eating-selection-fruit-vegetable-seeds-superfood-cereals-leaf-vegetable-on-722718082.jpg'),
+    new Recipe('title2', 'the second item in the list', 'https://image.shutterstock.com/display_pic_with_logo/136306/722718082/stock-photo-healthy-food-clean-eating-selection-fruit-vegetable-seeds-superfood-cereals-leaf-vegetable-on-722718082.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
