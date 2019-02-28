e689db2b4b09508e82a353152cdafe8bf248a7fd
Shouvik Bhuiyan <shouvik.bhuiyan@sapient.com>
2019-02-28 11:30:31 +0530

    experimenting angular lifecycle methods

--- firstComponent/src/app/app.component.ts
+++ firstComponent/src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, 
+          OnChanges,
+          OnInit, 
+          DoCheck, 
+          AfterContentInit, 
+          AfterContentChecked,
+          AfterViewInit,
+          AfterViewChecked,
+          OnDestroy,
+          SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +17,43 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'firstComponent';
   name = "initial value";
+
+  constructor() {
+    console.log('constructor is called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('OnChanges is called '+ changes);
+  }
+
+  ngOnInit() {
+    console.log('onInit is called');
+  }
+
+  ngDoCheck() {
+    console.log('doCheck is called ');
+  }
+
+  ngAfterContentInit() {
+    console.log('AfterContentInit is called');
+  }
+
+  ngAfterContentChecked() {
+    console.log('AfterContentChecked is called');
+  }
+
+  ngAfterViewInit() {
+    console.log('AfterViewInit is called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('AfterViewChecked is called');
+  }
+
+  ngOnDestroy() {
+    console.log('OnDestroy is called');
+  }
+
   onInputChange(event : Event) {
     this.name = (<HTMLInputElement>event.target).value;
   }
